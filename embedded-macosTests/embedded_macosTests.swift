d322ad5dc944a26dd29823170aec554527702701
Julian Torres <julian@excessive.dev>
2026-09-11 07:54:52 -0400

    Initial Commit

---
+++ embedded-macosTests/embedded_macosTests.swift
@@ -0,0 +1,17 @@
+//
+//  embedded_macosTests.swift
+//  embedded-macosTests
+//
+//  Created by Julian Torres on 9/11/26.
+//
+
+import Testing
+@testable import embedded_macos
+
+struct embedded_macosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
